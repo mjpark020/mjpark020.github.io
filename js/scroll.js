92baca69121886db9e957202bfd99ecd37fad540
mjpark020 <97079041+mjpark020@users.noreply.github.com>
2022-01-13 16:27:19 +0900

    Initial commit

--- js/scroll.js
+++ js/scroll.js
@@ -1,15 +1,88 @@
-$(function () {
-    var rollHeader = 100;
-    $(window).scroll(function () {
-      var scroll = getCurrentScroll();
-      if (scroll >= rollHeader) {
-        $('nav').addClass('roll');
-      }
-      else {
-        $('nav').removeClass('roll');
-      }
-    });
-    function getCurrentScroll() {
-      return window.pageYOffset || document.documentElement.scrollTop;
+// 메뉴 버튼 눌림 여부
+var menuOn = false;
+
+// 반응형 화면 크기
+var windowWidth = window.matchMedia("screen and (max-width: 982px)");
+
+// 메뉴 클릭 함수
+function menuOnClick() {
+  if(windowWidth.matches) {
+    if(menuOn) {
+      $('nav ul').css({"display":"block"});
+      $('nav').css({
+        "height": "50px",
+        "background-color": "#333"
+      });
+    } else {
+      $('nav ul').css({"display":"none"});
+      $('nav').css({
+        "height": "100px",
+        "background-color": "transparent"
+      });
     }
+  }
+}
+
+// window.onload
+$(function () {
+  ///////////////////////////////////////////////////////
+  // 상단바 조정
+  var rollHeader = 100;
+  $(window).scroll(function () {
+    menuOn = false;
+    menuOnClick();
+    var scroll = getCurrentScroll();
+    if (scroll >= rollHeader) {
+      $('nav').addClass('roll');
+    }
+    else {
+      $('nav').removeClass('roll');
+    }
+  });
+  function getCurrentScroll() {
+    return window.pageYOffset || document.documentElement.scrollTop;
+  }
+
+
+  ///////////////////////////////////////////////////////
+  // nav 메뉴 이동
+  $('nav li').eq(0).on('click', function() {
+    window.scroll({top: 0, behavior: 'smooth'});
+  });
+
+  $('nav li').eq(1).on('click', function() {
+    document.getElementById('introduce').scrollIntoView({behavior: "smooth"});
+  });
+
+  $('nav li').eq(2).on('click', function() {
+    document.getElementById('skills').scrollIntoView({behavior: "smooth"});
+  });
+
+  $('nav li').eq(3).on('click', function() {
+    document.getElementById('projects').scrollIntoView({behavior: "smooth"});
+  });
+
+
+  ///////////////////////////////////////////////////////
+  //nav menu button
+  $('body').on('click', function(e) {
+    if($(e.target).hasClass('menu')) {
+      menuOn = !menuOn;
+      console.log('menuOn: '+menuOn);
+      menuOnClick();
+      return false;
+    }
+    menuOn = false;
+    menuOnClick();
+    console.log('.menu를 제외한 body');
+  });
+  
+  
+
+  ///////////////////////////////////////////////////////
+  // scroll down 버튼
+  $('#top button').on('click', function() {
+    document.getElementById('introduce').scrollIntoView({behavior: "smooth"});
+  });
+
 });
